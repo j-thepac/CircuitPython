7490aa1f4673c2ec742cc44d395a69ee628908c2
Deepak <j.thepac@gmail.com>
2021-02-06 15:33:53 +0530

    code added

--- Seeeduino_Xiao/Blink.py
+++ Seeeduino_Xiao/Blink.py
@@ -1,15 +1,23 @@
+
+#-----------
+#A1 - A10 can be used as well if u use a separate LED and a Resistor 100 - 400 ohms refer below for calculations
 import time
 import board
 from digitalio import DigitalInOut,Direction
 
-
-led = DigitalInOut(board.D13) #D13 is a built in LED
-
-#A1 - A10 can be used as well if u use a separate LED and a Resistor 100 - 400 ohms refer below for calculations
+led = DigitalInOut(board.D13) #A1
 led.direction=Direction.OUTPUT
+
+led_2 = DigitalInOut(board.A1) #A1
+led_2.direction=Direction.OUTPUT
 
 while True:
     led.value = True
+    led_2.value = True
     time.sleep(1)
+
     led.value=False
+    led_2.value=False
     time.sleep(1)
+
+
